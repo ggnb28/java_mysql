3b06cb13bdd45b75da35fd75063fc80523ebefa5
ggnb28 <tndud217@gmail.com>
2022-09-05 10:02:52 +0900

    java_mysql

---
+++ src/MiniProhectConnectMysql.java
@@ -0,0 +1,15 @@
+public class MiniProhectConnectMysql {
+
+    public static void main(String[] arg){
+        // 1. My Sql 실행
+        // 2. Database - Connect to Database 
+        // 3. Default Schema에 'world' 입력 후 Ok 버튼
+        // 4. world Schema에 볼드체로 연결이 되었는지 확인
+        // 5. 쿼리 추가
+        // 6. select * from city 를 실행 후 테이블이 Action Output에 오류가 안나는지 확인
+        // 7. Table에 Column들이 제대로 나왔는지 확인 (ID, Name, CountryCode, District, Population)
+        
+
+    }
+    
+}
